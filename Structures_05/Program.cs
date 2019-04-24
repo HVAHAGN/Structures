591837cab9a0d71f016015605ca8f885fc2f6c65
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 15:39:56 +0400

    Структуры могут содержать статические члены

---
+++ Structures_05/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures_05
+{
+    // Структуры.
+
+    // Структуры могут содержать статические члены.
+
+    // Статические структуры недопустимы.
+    struct MyStruct
+    {
+        public static int Field
+        {
+            get;
+            set;
+        }
+
+        public static void Show()
+        {
+            Console.WriteLine(Field);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Инициализация статических полей необязательна.
+            //MyStruct.Field = 1;
+
+            MyStruct.Show();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
+
