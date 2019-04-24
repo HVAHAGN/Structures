c76436c1496abe658a5cf3890d3bf00b7c5a9a5f
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 15:43:06 +0400

    Статический конструктор всегда отрабатывает первым.

---
+++ Structures_06/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures_06
+{
+    struct MyStruct
+    {// Структуры. Статический конструктор.
+        public int field;
+
+        // Статический конструктор всегда отрабатывает первым.
+        static MyStruct()
+        {
+            Console.WriteLine("Static Constructor");
+        }
+
+        // Если в структуре имеется пользовательский конструктор, то требуется в нем инициализировать все поля.
+        public MyStruct(int value)
+        {
+            Console.WriteLine("Constructor");
+            this.field = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Создание экземпляра структурного типа, с вызовом пользовательского конструктора.
+            MyStruct instance = new MyStruct { field = 0 };
+
+            Console.WriteLine(instance.field);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
