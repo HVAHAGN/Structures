13e7c39c7f966ad80c6718c5e96dcbeb30e3086b
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 16:23:45 +0400

    Nested structures.

---
+++ St_Nested_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace St_Nested_01
+{
+    // Nested structures.
+
+    struct MyStruct
+    {
+        public struct Nested
+        {
+            public void Method()
+            {
+                Console.WriteLine("Nested");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyStruct.Nested instance = new MyStruct.Nested();
+            instance.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
