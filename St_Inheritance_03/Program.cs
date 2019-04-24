c5eb014cde55d19924c69c64aec22c0cf10c1bbd
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 16:19:59 +0400

    От структур нельзя наследоваться.

---
+++ St_Inheritance_03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace St_Inheritance_03
+{
+    // Структуры. Наследование.
+
+    // От структур нельзя наследоваться.
+    struct MyStruct
+    {
+        // Структуры не могут иметь protected членов.
+        //protected int field;
+    }
+
+    class MyClass //: MyStruct
+    {
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("От структур нельзя наследоваться.");
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
