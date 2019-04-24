642fc07635ded162f5a3c98bc54990368ece2126
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 16:09:11 +0400

    Наследование структур разрешено только от интерфейсов.

---
+++ St_Inheritance_01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace St_Inheritance_01
+{
+    // Структуры. Наследование.
+
+    // Наследование структур разрешено только от интерфейсов.
+    // Наследование структур, от классов и структур запрещено.
+
+    namespace Inheritance
+    {
+        interface IInterface
+        {
+            void Method();
+        }
+
+        struct MyStruct : IInterface
+        {
+            public void Method()
+            {
+                Console.WriteLine("Method");
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                MyStruct instance;
+
+                instance.Method();
+
+                // Delay.
+                Console.ReadKey();
+            }
+        }
+    }
+}
