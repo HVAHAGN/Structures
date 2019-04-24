863cc2ba318e413babc636fe742d9bf557df5e01
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 14:57:08 +0400

    Cоздание экземпляра структурного типа, без вызова конструктора

--- Structures/Program.cs
+++ Structures/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace Structures
 {
+    // Структуры.
+
+    // В структурах нельзя инициализировать поля непосредственно в месте создания.
+    struct MyStruct
+    {
+        public int field;
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Создание экземпляра структурного типа, без вызова конструктора.
+            MyStruct instance;
+
+            instance.field = 1; // Закомментировать.
+
+            // Попытка вывода значения неинициализированного поля приведет к ошибке.
+            Console.WriteLine(instance.field);
+
+            // Delay.
+            Console.ReadKey();
         }
     }
 }
