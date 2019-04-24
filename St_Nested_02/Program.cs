1aaf100a17e4319e60c38bfb2ddd13c9ab3f9241
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 16:29:55 +0400

    Структуры могут содержать вложенные классы.

---
+++ St_Nested_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace St_Nested_02
+{
+    // Nested structures.
+
+    // Структуры могут содержать вложенные классы.
+    struct MyStruct
+    {
+        public class Nested
+        {
+            public void Method()
+            {
+                Console.WriteLine("Nested");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyStruct.Nested instance = new MyStruct.Nested();
+            instance.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
